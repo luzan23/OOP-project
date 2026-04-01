584bd4b95a2f823fd2e8b93b100e085f59de537b
atiasyaa <atiasyaa@post.bgu.ac.il>
2026-04-01 12:35:44 +0300

    add tests

---
+++ src/tests/ScalarTest.java
@@ -0,0 +1,42 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+public class ScalarTest {
+    @Test
+    public void  testAdd(){
+
+    }
+    @Test
+    public void  testMul(){
+
+    }
+    @Test
+    public void testNeg(){
+
+    }
+    @Test
+    public void testPower(){
+
+    }
+    @Test
+    public void testEquals(){
+
+    }
+     @Test
+    public void testTostring(){
+
+     }
+
+
+
+
+
+
+
+
+
+
+
+
+}
