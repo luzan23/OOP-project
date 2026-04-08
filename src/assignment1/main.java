6f85f65bf1e549d72341baca2d9d7603e79611c2
atiasyaa <atiasyaa@post.bgu.ac.il>
2026-04-08 19:13:54 +0300

    made jar and main

--- src/assignment1/main.java
+++ src/assignment1/main.java
@@ -2,6 +2,8 @@ package assignment1;
 
 public class main {
     public static void main(String[] args){
+        Polynomial x = new Polynomial("2 3 4");
+        System.out.println(x.add(new Polynomial("2 5 6")));
 
     }
 
