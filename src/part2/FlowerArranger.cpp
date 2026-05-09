d91e402cb55d94dd7df0e63230eea02b43832b2e
atiasyaa <atiasyaa@post.bgu.ac.il>
2026-05-09 16:25:52 +0300

    all

--- src/part2/FlowerArranger.cpp
+++ src/part2/FlowerArranger.cpp
@@ -4,6 +4,10 @@
 #include <vector>
 #include <string>
 #include <iostream>
+FlowerArranger::FlowerArranger(std::string name) : Person(name)
+{
+
+}
 
 void FlowerArranger::arrangeFlowers(FlowersBouquet* bouquet){
 	std::cout << "Flower Arranger " << getName() << " arranges flowers" << std::endl;
--- src/part2/Grower.cpp
+++ src/part2/Grower.cpp
@@ -5,12 +5,12 @@
 #include "Person.h"
 #include <iostream> 
 Grower::Grower(std::string name, Gardener* gardener)
-	: Person(name), gardener(gardener)}
+	: Person(name), gardener(gardener)
 	{
 	}
-	FlowersBouquet*  Grower::prepareOrder(std::vector<std::string>) flowers){
+	FlowersBouquet*  Grower::prepareOrder(std::vector<std::string> flowers) {
 		std::cout << "Grower " << getName() << " forwards the request to" << "Gardener" << gardener->getName() << std::endl;
 		FlowersBouquet* bouquet = gardener->prepareBouquet(flowers);
-		std::cout << "Grower " << name << " returns flowers to Wholesaler." << std::endl;
+		std::cout << "Grower " << getName() << " returns flowers to Wholesaler." << std::endl;
 		return bouquet;
 	}
